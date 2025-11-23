3c0106b7b4ba323c2b0ed7bfddcbd62ecf0eed02
Rajath-Git01 <Officer.int@gmail.com>
2025-11-23 18:35:23 +0530

    Initial Change

---
+++ src/main/java/com/intertecsys/App.java
@@ -0,0 +1,13 @@
+package com.intertecsys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
